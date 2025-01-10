ee061edc265ce67f73822ab773a0abedcae89cee
Lars <lars@goodchee.com>
2025-01-10 14:23:47 -0500

    cleanup and removal of browsersync

--- src/js/backend.js
+++ src/js/backend.js
@@ -4,14 +4,5 @@ import './backend/registerBlockStyles';
 // Unregister blocks
 import './backend/unregisterBlocks';
 
-// If a button has a url and placeholder but no text, set the placeholder as the text on save
-import './backend/setButtonTextFallbackToPlaceholder';
-
 // Add a max-width setting to paragraphs and headings
 import './backend/attributeMaxWidth';
-
-// Add an icon dropdown to paragraphs
-import './backend/attributeParagraphIcon';
-
-// Add an icon attribute to lists and buttons
-import './backend/attributeIcon';
