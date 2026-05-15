e56a68e4ae93a27336d0f84448df8851755c87d5
Matthew James <matt@goodchee.com>
2026-05-15 13:26:49 -0700

    add Disable XML-RPC

--- includes/hooks-wordpress.php
+++ includes/hooks-wordpress.php
@@ -5,6 +5,9 @@ namespace CHEENAMESPACE;
 /* Init
 ========================================================= */
 
+// Disable XML-RPC
+add_filter("xmlrpc_enabled", "__return_false");
+
 // Setup
 add_action('after_setup_theme', 'CHEENAMESPACE\setup');
 
